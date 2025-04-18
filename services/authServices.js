79f65634f1d9e56f32a6265430e6e7a50769f13c
Olesia Vysotina <vysotina2002@gmail.com>
2025-04-18 17:45:32 +0200

    feat: implement user registration functionality with email conflict check

--- services/authServices.js
+++ services/authServices.js
@@ -1,5 +1,15 @@
 import User from "../db/models/User.js";
+import HttpError from "../helpers/HttpError.js";
+import { conflictExistsEmailMessageInUse } from "../constants/messages.js";
 
-export const registerUser = async (userData) => {};
+export const registerUser = async (userData) => {
+  const { email, password } = userData;
+  const existingUser = await User.findOne({ where: { email } });
+  if (existingUser) {
+    throw HttpError(409, conflictExistsEmailMessageInUse);
+  }
+  const newUser = await User.create(userData);
+  return newUser;
+};
 export const loginUser = async (userData) => {};
 export const logoutUser = async (userId) => {};
