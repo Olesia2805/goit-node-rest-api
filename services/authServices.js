1e4ae99297e6cbe6e88a10e519f3dad46a16422e
Olesia Vysotina <vysotina2002@gmail.com>
2025-04-24 22:34:31 +0200

    add bcrypt for password hashing in user registration

--- services/authServices.js
+++ services/authServices.js
@@ -1,3 +1,5 @@
+import bcrypt from "bcrypt";
+
 import User from "../db/models/User.js";
 import HttpError from "../helpers/HttpError.js";
 import { conflictExistsEmailMessageInUse } from "../constants/messages.js";
@@ -5,10 +7,14 @@ import { conflictExistsEmailMessageInUse } from "../constants/messages.js";
 export const registerUser = async (userData) => {
   const { email, password } = userData;
   const existingUser = await User.findOne({ where: { email } });
+
   if (existingUser) {
     throw HttpError(409, conflictExistsEmailMessageInUse);
   }
-  const newUser = await User.create(userData);
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...userData, password: hashedPassword });
   return newUser;
 };
 export const loginUser = async (userData) => {};
