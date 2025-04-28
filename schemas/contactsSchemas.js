7cf6f6c40221501a142e3632e4d036a351f3a3fa
Olesia Vysotina <vysotina2002@gmail.com>
2025-04-28 22:21:22 +0200

    fix: update updateContactSchema to make fields optional and require at least one field

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -33,23 +33,28 @@ export const createContactSchema = Joi.object({
 });
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(50).required().messages({
+  name: Joi.string().min(3).max(50).messages({
     "string.min": "Name must be at least 3 characters long",
     "string.max": "Name must be at most 50 characters long",
     "string.empty": nameNotEmptyMessage,
     "string.required": nameIsRequiredMessage,
   }),
-  email: Joi.string().email().required().messages({
+  email: Joi.string().email().messages({
     "string.email": emailValidationMessage,
     "string.empty": emailNotEmptyMessage,
     "string.required": emailIsRequiredMessage,
   }),
-  phone: Joi.string().pattern(phoneRegex).required().messages({
+  phone: Joi.string().pattern(phoneRegex).messages({
     "string.pattern.base": phoneValidationMessage,
     "string.empty": phoneNotEmptyMessage,
     "string.required": phoneIsRequiredMessage,
   }),
-});
+})
+  .or("name", "email", "phone")
+  .messages({
+    "object.missing":
+      "At least one field (name, email, or phone) must be provided",
+  });
 
 export const updateStatusContactSchema = Joi.object({
   favorite: Joi.boolean().required().messages({
